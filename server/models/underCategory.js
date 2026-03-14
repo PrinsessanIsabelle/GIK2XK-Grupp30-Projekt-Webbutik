931271ff5e6d6cd945c5acc9bb57fb6820e6a7c8
Isabelle Lindberg <h24islin@du.se>
2026-03-14 16:11:41 +0100

    Rename model definitions for consistency: 'cartrow' from'cartRow' and 'underCategory' from 'productCategory'

--- server/models/underCategory.js
+++ server/models/underCategory.js
@@ -1,10 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define(
-<<<<<<< HEAD:server/models/underCategory.js
     'underCategory', {
-=======
-    'productCategory', {
->>>>>>> b6da9ab0da38621e80b4c14a1a7cb14d00ec4c9f:server/models/productcategory.js
         id:{ 
                 type: DataTypes.INTEGER,
                 primaryKey: true,
