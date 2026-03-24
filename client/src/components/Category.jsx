15ae34f23dfb028b8f5751908cdd5fd2554d6115
h24axeal <h24axeal@du.se>
2026-03-24 17:03:51 +0100

    ändrat katogori knappen inne på produkten

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -5,7 +5,19 @@ function Category( {text} ) {
    
     return (
     <Link to={`/categories/${text}/products`}>
-      <Chip label={text}></Chip>
+<Chip
+  label={text}
+  sx={{
+    backgroundColor: '#b2ff59',
+    color: '#000',
+    border: '2px solid #b2ff59',
+    fontWeight: 'bold',
+    '&:hover': {
+      backgroundColor: '#9cef5d',
+      borderColor: '#9cef5d'
+    }
+  }}
+/>
     </Link>
   );
     
